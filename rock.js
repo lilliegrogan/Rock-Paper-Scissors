8ee217cd25358d2aa878af809e6222d5ad684ef5
LunarDeluged <lilliebohr@gmail.com>
2022-01-21 16:05:16 -0600

    finally

--- rock.js
+++ rock.js
@@ -1,67 +1,61 @@
-// computerPlay function has the computer pick a random number 0-2 and assigns it to rock paper or scissors
 function computerPlay() {
     const randomNumber = Math.floor(Math.random() * 3);
       if (randomNumber === 0){
+        console.log("Computer played: rock");
         return "rock";
     } else if (randomNumber === 1){
-        return "paper";
+        console.log("Computer played: paper");
+       return "paper";
     } else if (randomNumber === 2){
-        return "scissors";
+        console.log("Computer played: scissors")
+       return "scissors";
     }
    
 }
  
-
-
 //userChoice function lets the player enter rock paper or scissors
 
 function userChoice(){
     let user = prompt("Enter your choice ");
     user = user.toLowerCase(); 
-    console.log("Your choice is: " + user);  
-
-   
+    if (user === "rock" || user === "paper" || user === "scissors"){
+        console.log("Your choice is: " + user);
+    } else {
+        console.log("invalid choice");
+    }
+    return user
+    //console.log("Your choice is: " + user);  
 }
 
-userChoice();
-console.log("Computer played: " + computerPlay());
+//userChoice();
+//computerPlay();
+//console.log("Computer played: " + computerPlay());
 
 
 
-/*let playerSelection = userChoice();
+let playerSelection = userChoice();
 
-let computerSelection = computerPlay();*/
+let computerSelection = computerPlay();
+//console.log(playerSelection)
+//console.log(computerSelection)
 
-
-
-
-
-
-
-
-
-
-
-/*function playRound(playerSelection, computerSelection){
-     if (playerSelection === computerSelection){
-        return 'Tie';
-    } else if (playerSelection === 'rock' && computerSelection === 'paper'){
-        return"You lose";
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-        return "You lose";
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
-        return "You lose";
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-        return "You win";
-    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
-        return "You win";
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-        return "You win";
+// playRound function tells the program the rules behind winning and losing
+function playRound(){
+    if (playerSelection === computerSelection){
+        console.log("Tie");
+    } else if (playerSelection === "rock" && computerSelection === "paper"){
+        console.log("You lose");
+    } else if (playerSelection === "paper" && computerSelection === "scissors"){
+        console.log("You lose");
+    } else if (playerSelection === "scissors" && computerSelection === "rock"){
+        console.log("You lose");
+    } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        console.log("You win");
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+        console.log("You win");
+    } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        console.log("You win");
     }    
 }
 
-
-
-
 playRound();
-console.log(playRound(playerSelection, computerSelection));*/ 
